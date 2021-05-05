fb45b25b37cd017f751523a9247243f78b29f6aa
gleal17 <guilhermelml@gmail.com>
2021-05-04 21:22:02 -0300

    front ci, adjustments

--- client/server.js
+++ client/server.js
@@ -2,10 +2,12 @@ const
   express = require('express'),
   serveStatic = require('serve-static'),
   history = require('connect-history-api-fallback'),
-  port = process.env.PORT || 5000
+  port = process.env.PORT || 3000
 
 const app = express()
-
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host)
 app.use(history())
 app.use(serveStatic(__dirname + '/dist/spa'))
 app.listen(port)
